a1ecda645dece1ad76a26da925ede098e6a24cbf
Gavin Sargent <gavin.sargent3535@gmail.com>
2023-03-30 12:44:06 -0600

    finished the get request from front to back

--- public/schedule.js
+++ public/schedule.js
@@ -1,31 +1,51 @@
-const viewQuotesForm = document.getElementById('view-quotes-form')
-const quotesDiv = document.getElementById('my-quotes')
-const quotePhone = document.getElementById('view-quotes-phone')
+let viewQuotesForm = document.getElementById('view-quotes-form')
+let quotesDiv = document.getElementById('my-quotes-container')
+let quotePhone = document.getElementById('view-quotes-phone')
 
 
 
-function getQuotes(e){
+viewQuotesForm.addEventListener('submit', (event) => {
+    event.preventDefault()
     
-
-    axios.get(`http://localhost:6500/quotes/${quotePhone}`)
+    let actQuotePhone = quotePhone.value
+    
+    axios.get("http://localhost:6500/quotes/" + actQuotePhone)
     .then(res => { 
-        console.log(res.data)
-        for (let i = 0; i < res.data.length; i++) {
-            const { quote_id, phone, date, time, service } = res.data
-            const quoteElem = 
-            `<div id="quote-${quote_id}">
-                <p class="quote-phone">Phone: ${phone}</p>
-                <p class="quote-date">Date: ${date}</p>
-                <p class="quote-time">Time: ${time}</p>
-                <p class="quote-service">Service: ${service}</p>
-            </div>`
+        // console.log(res.data)
+            quotesDiv.innerHTML = ''
+        
+            if (res.data.length === 0) {
+                let noQuotesMsg = document.createElement('h4')
+                noQuotesMsg.innerHTML = 'There are no quotes associated with that number'
+                quotesDiv.appendChild(noQuotesMsg)
+                return
+            }
+            for (let i = 0; i < res.data.length; i++) {
+                let quoteObj = {phone: res.data[i].phone,
+                quoteId: res.data[i].quote_id,
+                date: res.data[i].date,
+                time: res.data[i].time,
+                service: res.data[i].service}
 
-            quotesDiv.innerHTML += quoteElem    
-        }       
+                let individualQuote = document.createElement('div')
+                individualQuote.classList.add('individualQuote')
+                individualQuote.setAttribute("id", quoteObj.quoteId)
+
+                let quotePhone = document.createElement('p')
+                let quoteDate = document.createElement('p')
+                let quoteTime = document.createElement('p')
+                let quoteService = document.createElement('p')
+
+                quotePhone.innerHTML = "Phone: " + quoteObj.phone
+                quoteDate.innerHTML = "Date: " + quoteObj.date
+                quoteTime.innerHTML = "Time: " + quoteObj.time
+                quoteService.innerHTML = "Service: " + quoteObj.service
+
+                quotesDiv.appendChild(individualQuote)
+                individualQuote.appendChild(quotePhone)
+                individualQuote.appendChild(quoteDate)
+                individualQuote.appendChild(quoteTime)
+                individualQuote.appendChild(quoteService)
+            }                   
     }).catch((err)=> console.log(`something bad happened with the backend`, err))
-}
-
-
-
-
-viewQuotesForm.addEventListener("submit", getQuotes())
+})
--- server/controller.js
+++ server/controller.js
@@ -15,14 +15,14 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
 module.exports = {
     getMyQuotes: (req, res) => {
         const {quotePhone} = req.params
-        console.log("THIS IS THE PHONE ", quotePhone)
+        // console.log("THIS IS THE PHONE ", quotePhone)
         sequelize.query(`
             SELECT quotes.quote_id, users.phone, quotes.date, quotes.time, quotes.service
             FROM quotes
             JOIN users ON users.user_id = quotes.user_id
             WHERE users.phone = '${quotePhone}' 
-        `).then(dbRes => {res.status(200).send(dbRes[0])
-        console.log(dbRes[0])})
+        `)
+        .then(dbRes => {res.status(200).send(dbRes[0])})
         .catch(err => console.log("something went wrong with the database", err))
     },
 
